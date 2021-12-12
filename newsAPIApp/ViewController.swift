ce7868c02cf0264591fa1edce950015786832841
yuta-2525 <Yuta1091.3150@gmail.com>
2021-12-12 09:49:50 +0900

    Initial Commit

---
+++ newsAPIApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  newsAPIApp
+//
+//  Created by 佐藤勇太 on 2021/12/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
